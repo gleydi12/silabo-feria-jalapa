dda899e18891d3a91c123d24e49b0e7f34c844b6
Carlos Andres Perez Ubeda <descarlos2013@gmail.com>
2024-10-13 00:17:44 -0600

    Register global utils from Utils folder
    Added functionality to scan and require all PHP files in the Utils folder, allowing utility functions to be available globally. This ensures that any helper functions in the Utils directory can be utilized across the application without the need for individual imports.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\File;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,5 +22,16 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Model::unguard();
+
+        // Register the utils from Utils folder to expose globally
+        if (file_exists(app_path('Utils'))) {
+            $files = scandir(app_path('Utils'));
+
+            foreach ($files as $file) {
+                if ($file !== '.' && $file !== '..' && pathinfo($file, PATHINFO_EXTENSION) === 'php') {
+                    require_once app_path('Utils/'.$file);
+                }
+            }
+        }
     }
 }
