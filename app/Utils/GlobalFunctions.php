2257630f6d339d74706a5d2ab0c6eb1eb6510b4e
Carlos Andres Perez Ubeda <descarlos2013@gmail.com>
2024-10-13 00:18:20 -0600

    Add global utility functions for user types, sedes, and email verification
    Introduced utility functions `tipos_usuarios`, `select_sedes`, `verify_if_emails_is_taken`, and `isAdmin` in the `GlobalFunctions.php` file. These functions enhance reusability and simplify common tasks related to user type definitions, Sede selection, and email verification logic.

---
+++ app/Utils/GlobalFunctions.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Utils;
+
+use App\Models\Sede;
+use App\Models\User;
+use Filament\Forms\Components\Select;
+use Filament\Notifications\Notification;
+
+function tipos_usuarios(): array
+{
+    return [
+        1 => 'ADMIN',
+        25 => 'OFICINAS',
+        50 => 'DOCENTES',
+    ];
+}
+
+function select_sedes(): Select
+{
+    return Select::make('sede_id')
+        ->hidden(! auth()->user()->is_admin)
+        ->label('Sede')
+        ->searchable()
+        ->options(fn () => Sede::pluck('nombre', 'id'));
+}
+
+function verify_if_emails_is_taken($object): bool
+{
+    // Verificar si el correo del usuario ya existe
+    if (User::where('email', $object->data['email'])->exists()) {
+        Notification::make()
+            ->warning()
+            ->title('El correo ya existe!')
+            ->body('El correo ya existe en la base de datos.')
+            ->persistent()
+            ->send();
+        return true;
+    }
+    return false;
+}
+
+function isAdmin(): bool
+{
+    return auth()->user()->tipo === User::ADMIN;
+}
