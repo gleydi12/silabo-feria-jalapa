ebb0ebe1940bb8ed1d1207b9463cc046a6573274
Carlos Andres Perez Ubeda <descarlos2013@gmail.com>
2024-10-16 23:36:08 -0600

    Add seeding of Carreras table
    Include predefined list of "carreras" (study programs) to the seeding process in DatabaseSeeder. Each "carrera" is created with a fixed duration of 5 years and relevant name and code.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Carrera;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -37,5 +38,33 @@ class DatabaseSeeder extends Seeder
         ])->each(function ($user) {
             $user->assignRole('profesor');
         });
+
+        //Crear carreras de prueba
+        $carreras = [
+            ['codigo' => 3401, 'nombre' => 'Técnico Superior en Educación Física, Deportes y Recreación'],
+            ['codigo' => 3122, 'nombre' => 'Ciencias de la Educación con Mención en Sociales'],
+            ['codigo' => 3123, 'nombre' => 'Ciencias de la Educación con Mención en Ciencias Naturales'],
+            ['codigo' => 3120, 'nombre' => 'Ciencias de la Educación con Mención en Español'],
+            ['codigo' => 3101, 'nombre' => 'Pedagogía'],
+            ['codigo' => 1101, 'nombre' => 'Teología'],
+            ['codigo' => 3301, 'nombre' => 'Inglés'],
+            ['codigo' => 7104, 'nombre' => 'Psicología Clínica'],
+            ['codigo' => 4101, 'nombre' => 'Derecho'],
+            ['codigo' => 5101, 'nombre' => 'Administración de Empresas'],
+            ['codigo' => 5201, 'nombre' => 'Contaduría Pública y Auditoría'],
+            ['codigo' => 5301, 'nombre' => 'Banca y Finanzas'],
+            ['codigo' => 2102, 'nombre' => 'Ingeniería de Sistemas'],
+            ['codigo' => 8101, 'nombre' => 'Ingeniería en Gerencia Agropecuaria'],
+            ['codigo' => 7101, 'nombre' => 'Técnico Superior en Enfermería General'],
+            ['codigo' => 7201, 'nombre' => 'Farmacia'],
+            ['codigo' => 7102, 'nombre' => 'Enfermería'],
+        ];
+
+        foreach ($carreras as $index => $carrera) {
+            Carrera::create([
+                'duracion' => 5,
+                'nombre' => $carrera['nombre'],
+            ]);
+        }
     }
 }
